f04bcc817367e96e0ed31868de29e6d7b5d1bb01
Johannes Singler <johannes@singler.name>
2014-08-26 22:00:26 +0200

    Better test.

--- test/risontest.dart
+++ test/risontest.dart
@@ -3,7 +3,7 @@ import 'package:rison/rison.dart';
 
 main() {
   test('RISON test', () {
-    Map mapIn = { 'x' : 1 };
+    Map mapIn = { 'x': 1, 'y': { 'y1': 0.123 }, 'z': [ 'a', 'b', 'c']};
 
     String rison = mapToRison(mapIn);
 
@@ -11,7 +11,9 @@ main() {
 
     Map mapOut = risonToMap(rison);
 
-    expect(mapOut, mapIn);
     expect(mapOut is Map, true);
+    mapOut['y'] = new Map.from(jsObjectToMap(mapOut['y']));
+    mapOut['z'] = jsObjectToList(mapOut['z']);
+    expect(mapOut, mapIn);
   });
 }
