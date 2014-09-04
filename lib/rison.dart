770ada386c9e1cedb53b557cd5d11fc405e4d801
Johannes Singler <johannes@singler.name>
2014-09-04 17:58:16 +0200

    Put everything into RisonStateKeeper class.

--- lib/rison.dart
+++ lib/rison.dart
@@ -6,92 +6,92 @@ import 'dart:convert';
 
 import 'package:js_wrapping/js_wrapping.dart';
 
-String toRison(Object input) => context['rison'].callMethod('encode', [ (input is Map || input is Iterable) ? new JsObject.jsify(input) : input ]);
-Object risonToObject(String rison) => context['rison'].callMethod('decode', [ rison ]);
-Map jsObjectToMap(JsObject jsObject) => TypedJsMap.$wrap(jsObject);
-List jsObjectToList(JsObject jsObject) => jsObject as JsArray;
-
-String preencode(String text) {
-  byteToHex(int byte, StringBuffer buffer) {
-    const String hex = '0123456789ABCDEF';
-    buffer.write('%');
-    buffer.writeCharCode(hex.codeUnitAt(byte >> 4));
-    buffer.writeCharCode(hex.codeUnitAt(byte & 0x0f));
-  }
-  StringBuffer result = new StringBuffer();
-  for (int i = 0; i < text.length; i++) {
-    int codeUnit = text.codeUnitAt(i);
-    if (codeUnit > 127) {
-      String utf16Encoded = new String.fromCharCode(codeUnit);
-      List<int> utf8Encoded = UTF8.encode(utf16Encoded);
-      utf8Encoded.forEach((int c) { byteToHex(c, result); });
-    } else {
-      result.writeCharCode(codeUnit);
-    }
-  }
-  return result.toString();
-}
-
-Object fromRison(String rison) {
-  Object object = risonToObject(rison);
-
-  return convertObject(object, false);
-}
-
-Object fromRisonRecursive(String rison) {
-  Object object = risonToObject(rison);
-
-  return convertObject(object, true);
-}
-
-Object convertObject(Object object, bool recursive) {
-  if (object is JsArray) {
-    List list = jsObjectToList(object);
-    if (recursive)
-      convertArray(list);
-    return list;
-  }
-  else if (object is JsObject) {
-    Map map = jsObjectToMap(object);
-    if (recursive)
-      convertMap(map);
-    return map;
-  }
-  else
-    return object;
-}
-
-void convertMap(Map map) {
-  map.forEach((String key, Object value) {
-    map[key] = convertObject(map[key], true);
-  });
-}
-
-void convertArray(List array) {
-  for (int i = 0; i < array.length; ++i)
-    array[i] = convertObject(array[i], true);
-}
-
-void printMap(Map map) {
-  print(mapToString(map));
-}
-String mapToString(Map map) {
-  String result = '';
-  map.forEach((k, v) {
-    String value = v.toString();
-    if (v is JsObject)
-      value = "( ${mapToString(jsObjectToMap(v))} )";
-    result = "$result, $k : $value";
-  });
-  return result;
-}
-
 abstract class StateKeeper {
   void listenToHash(void onHashChanged(String hash));
   void updateHash(String hash, bool replace);
 }
 
-class Rison implements StateKeeper {
+class RisonStateKeeper implements StateKeeper {
+  static String toRison(Object input) => context['rison'].callMethod('encode', [ (input is Map || input is Iterable) ? new JsObject.jsify(input) : input ]);
+  static Object risonToObject(String rison) => context['rison'].callMethod('decode', [ rison ]);
+  static Map jsObjectToMap(JsObject jsObject) => TypedJsMap.$wrap(jsObject);
+  static List jsObjectToList(JsObject jsObject) => jsObject as JsArray;
+  
+  static String preencode(String text) {
+    byteToHex(int byte, StringBuffer buffer) {
+      const String hex = '0123456789ABCDEF';
+      buffer.write('%');
+      buffer.writeCharCode(hex.codeUnitAt(byte >> 4));
+      buffer.writeCharCode(hex.codeUnitAt(byte & 0x0f));
+    }
+    StringBuffer result = new StringBuffer();
+    for (int i = 0; i < text.length; i++) {
+      int codeUnit = text.codeUnitAt(i);
+      if (codeUnit > 127) {
+        String utf16Encoded = new String.fromCharCode(codeUnit);
+        List<int> utf8Encoded = UTF8.encode(utf16Encoded);
+        utf8Encoded.forEach((int c) { byteToHex(c, result); });
+      } else {
+        result.writeCharCode(codeUnit);
+      }
+    }
+    return result.toString();
+  }
+  
+  static Object fromRison(String rison) {
+    Object object = risonToObject(rison);
+  
+    return convertObject(object, false);
+  }
+  
+  static Object fromRisonRecursive(String rison) {
+    Object object = risonToObject(rison);
+  
+    return convertObject(object, true);
+  }
+  
+  static Object convertObject(Object object, bool recursive) {
+    if (object is JsArray) {
+      List list = jsObjectToList(object);
+      if (recursive)
+        convertArray(list);
+      return list;
+    }
+    else if (object is JsObject) {
+      Map map = jsObjectToMap(object);
+      if (recursive)
+        convertMap(map);
+      return map;
+    }
+    else
+      return object;
+  }
+  
+  static void convertMap(Map map) {
+    map.forEach((String key, Object value) {
+      map[key] = convertObject(map[key], true);
+    });
+  }
+  
+  static void convertArray(List array) {
+    for (int i = 0; i < array.length; ++i)
+      array[i] = convertObject(array[i], true);
+  }
+  
+  static void printMap(Map map) {
+    print(mapToString(map));
+  }
+  static String mapToString(Map map) {
+    String result = '';
+    map.forEach((k, v) {
+      String value = v.toString();
+      if (v is JsObject)
+        value = "( ${mapToString(jsObjectToMap(v))} )";
+      result = "$result, $k : $value";
+    });
+    return result;
+  }
+
   void listenToHash(void onHashChanged(String hash)) {
     onHashChanged(hashWithoutTag);
     window.onHashChange.listen((HashChangeEvent e) {
@@ -99,7 +99,7 @@ class Rison implements StateKeeper {
     });
   }
 
-  String get hashWithoutTag {
+  static String get hashWithoutTag {
     String hash = (window.location.hash.length > 1) ? window.location.hash.substring(1) /* remove # */ : '';
     hash = preencode(hash);
     String decodedHash = Uri.decodeFull(hash);
